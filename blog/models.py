c606eb19fcc266b493723f382ab0cddcc272ea40
ye <blurrye@163.com>
2017-07-24 23:15:19 +0800

    ...1

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.core.urlresolvers import reverse
+
+
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super(PublishedManager, self).get_queryset().filter(status='published')
 
 
 class Post(models.Model):
@@ -17,6 +23,9 @@ class Post(models.Model):
     updated = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
 
+    objects = models.Manager()
+    published = PublishedManager()
+
     class Meta:
         verbose_name = '文章'
         verbose_name_plural = verbose_name
@@ -24,3 +33,12 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail',
+                       args=[
+                           self.publish.year,
+                           self.publish.strftime('%m'),
+                           self.publish.strftime('%d'),
+                           self.slug
+                       ])
